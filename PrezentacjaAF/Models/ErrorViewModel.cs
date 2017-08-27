cb9120f33663f523590f661efdb765a00fae54aa
Mateusz Jamrowski <mateuszjamrowski@outlook.com>
2017-08-27 12:01:27 +0200

    Dodaj pliki projektów.

---
+++ PrezentacjaAF/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PrezentacjaAF.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
